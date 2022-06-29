48f10f4f83dbdaf25c77d79fec59e0bb3b441327
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-29 10:46:21 -0700

    Add main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-pub mod lang;
+use git::{has_git, is_local_git};
+
 pub mod git;
+pub mod lang;
 
 fn main() {
-    println!("Hello, world!");
+    let git_dir = has_git("./".to_string());
+    if git_dir {
+        println!("Is git!");
+    } else {
+        println!("Is not git.");
+    }
+
+    let local = is_local_git("./".to_string());
+    if local {
+        println!("Is local!");
+    } else {
+        println!("Is not local.");
+    }
 }
